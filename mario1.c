a5e17a17cb503076928a55b734dae8daef3a3e74
Feleonel <63075242+Feleonel@users.noreply.github.com>
2020-09-09 23:25:45 -0300

    Create mario1.c

---
+++ mario1.c
@@ -0,0 +1,45 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //Ask for the size of the Pyramid
+
+    int n = get_int("Height: ");
+
+    while ( n < 1 || n > 8)
+    {
+        n = get_int("Height: ");
+    }
+    for (int j = 0; j < n ; j++)
+    {
+
+        //Specifies the number of empty spaces in each line
+        for (int i = 0; i < n - j - 1 ; i++)
+        {
+            printf(" ");
+        }
+
+        //Specifies the number of # in each line
+        for (int z = 0 ; z < j + 1 ; z++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        //Specifies the number of # in each line
+        for (int k = 0 ; k < j + 1 ; k++)
+        {
+            printf("#");
+        }
+
+        //Specifies the number of empty spaces in each line
+        for (int w = 0; w < n - j - 1 ; w++)
+        {
+            printf(" ");
+        }
+
+        printf("\n");
+    }
+}
